187007bfcdb834792c5ea54afb91c72b91470cd8
MountainClouds <974674855@qq.com>
2019-04-25 21:41:00 +0800

    66

---
+++ src/main/java/com/juck/mytoken/service/admin/mapper/TbSysUserExtendMapper.java
@@ -0,0 +1,9 @@
+package com.juck.mytoken.service.admin.mapper;
+
+import com.juck.mytoken.common.domain.TbSysUser;
+import org.springframework.stereotype.Repository;
+import tk.mybatis.mapper.MyMapper;
+
+@Repository
+public interface TbSysUserExtendMapper extends MyMapper<TbSysUser> {
+}
